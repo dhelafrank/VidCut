69b7086e92e75b19c2279a7175a7a8acdb06eb6f
dhelafrank <ubongabasijerome8@gmail.com>
2023-12-01 21:58:56 +0100

    Phrase Correction

--- routes/download.js
+++ routes/download.js
@@ -51,7 +51,7 @@ router.get('/video', (req, res) => {
 
             res.render("info", {
                 title: "VidCut | Download Video",
-                heading: "Click the below to download",
+                heading: "Click below to download",
                 content: `<a href="/download/start?path=${result.path}" class="btn btn-primary">Download <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="ml-2 h-4 w-4">
                 <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
                 <polyline points="7 10 12 15 17 10"></polyline>
