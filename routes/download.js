3784fde0b2910edc2afa5a4177bccd3edd8955a5
dhelafrank <ubongabasijerome8@gmail.com>
2023-12-02 09:36:16 +0100

    I don't know what seems wrong

--- routes/download.js
+++ routes/download.js
@@ -56,7 +56,6 @@ router.get('/video', (req, res) => {
 
     downloadFile(data, (result) => {
         if (result.status) {
-
             res.render("download", {
                 title: "VidCut | Download Video",
                 button:`<a href="/download/start?path=${result.path}" class="btn btn-lg btn-success download-btn">Download Now <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="ml-2 h-4 w-4">
