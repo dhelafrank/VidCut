24ed0c6ce8d2b68b1e5e65ebb67e26eaca638f2a
dhelafrank <ubongabasijerome8@gmail.com>
2023-12-03 00:34:40 +0100

    Attempting Mail Template background fix

--- services/mail-template.js
+++ services/mail-template.js
@@ -44,33 +44,31 @@ const errorMail = (code, heading, message) => {
 
 const videoReminderMail = (name, url) => {
   return `<html>
-<head>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-  <style>
-    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap');
-  </style>
-</head>
-<body style="font-family: 'Poppins', sans-serif; background-color: #f3f4f6;">
-  <div style="margin: 0 auto; max-width: 600px; padding: 20px; display:flex; align-items:center; justify-content:center; flex-direction:column;">
-  <h1 style="text-align:center">VidCut</h1>
-    <div style="background-color: #ffffff; padding: 20px; border-radius: 8px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); display:flex; align-items:center; justify-content:center; flex-direction:column;">
-      <h1 style="font-size: 24px; margin-bottom: 10px; color: #333;">Your ${name} video is ready</h1>
-      <h5 style="color: #047857; font-size: 18px;">Click the button below to download</h5>
-      <hr style="border-top: 1px solid #e2e8f0;">
-      <div style="margin-bottom: 15px;">
-        <p style="color: #718096; font-size: 16px;">
-          Your ${name} video is ready
-        </p>
-        <p style="color: #718096; font-size: 16px;">
-          Thank you for using our service
-        </p>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  </head>
+  <body style="background-color: #f3f4f6; display: flex; align-items: center; justify-content: center; height: 100vh;">
+    <div style="max-width: 600px; padding: 20px; display:flex; flex-direction:column; align-items:center; justify-content:center;">
+      <h1 style="text-align:center">VidCut</h1>
+      <div style="background-color: #ffffff; padding: 20px; border-radius: 8px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); display:flex; flex-direction:column; align-items:center; justify-content:center;">
+        <h1 style="font-size: 24px; margin-bottom: 10px; color: #333;">Your ${name} video is ready</h1>
+        <h5 style="color: #047857; font-size: 18px;">Click the button below to download</h5>
+        <hr style="border-top: 1px solid #e2e8f0;">
+        <div style="margin-bottom: 15px;">
+          <p style="color: #718096; font-size: 16px;">
+            Your ${name} video is ready
+          </p>
+          <p style="color: #718096; font-size: 16px;">
+            Thank you for using our service
+          </p>
+        </div>
+        <hr style="border-top: 1px solid #e2e8f0;">
+        <a style="display: inline-block; padding: 10px 15px; background-color: #3490dc; color: #ffffff; text-decoration: none; border-radius: 4px; font-size: 16px;" href="${url}" target="_blank">Download</a>
       </div>
-      <hr style="border-top: 1px solid #e2e8f0;">
-      <a style="display: inline-block; padding: 10px 15px; background-color: #3490dc; color: #ffffff; text-decoration: none; border-radius: 4px; font-size: 16px;" href="${url}" target="_blank">Download</a>
     </div>
-  </div>
-</body>
-</html>`
+  </body>
+  </html>
+  `
 }
 
 module.exports = {
